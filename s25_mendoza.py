1110c41cf2100fd472216e57947cf5c50313f4fc
RAS2159219 <RAS2159219@maricopa.edu>
2025-04-05 16:51:23 -0700

    new files

---
+++ s25_mendoza.py
@@ -0,0 +1,9 @@
+"""
+ Rashell Mendoza
+ Module 08 Programming Assignment
+ 
+ This program print quote from book.
+"""
+
+print("“Happy Hunger Games! And may the odds be ever in your favor.”")
+print("- Suzanne Collins, The Hunger Games")
